1744f495dec88bede652f3f1641ac41024a0400a
Ronaldmera <ronaldalme1230@gmail.com>
2024-07-19 11:39:45 -0500

    inicio

---
+++ Taller_Ronald_Java/src/Ejercicio1.java
@@ -0,0 +1,16 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        /*
+        1.	Encontrar, dentro de un arreglo de números enteros, un par de enteros que sumen un número dado. Ejemplo:
+         Si la entrada es el arreglo [8, 7, 2, 5, 3, 1] y la suma objetivo es 10, los pares encontrados deben ser (8,2) y (7,3). 2.
+         */
+
+        int [] enteros = {1,4,5,7};
+
+    }
+    public static void recorrido(int[] array){
+        for (int i = 0; i < array.length; i++) {
+
+        }
+    }
+}
